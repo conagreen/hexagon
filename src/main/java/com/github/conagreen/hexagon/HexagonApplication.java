86689687b4f26c9dabb1be9256b9f330227f6ab7
conagreen <icoqwee@gmail.com>
2021-11-25 00:13:31 +0900

    first commit

---
+++ src/main/java/com/github/conagreen/hexagon/HexagonApplication.java
@@ -0,0 +1,13 @@
+package com.github.conagreen.hexagon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexagonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HexagonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/conagreen/hexagon/HexagonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.conagreen.hexagon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexagonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
