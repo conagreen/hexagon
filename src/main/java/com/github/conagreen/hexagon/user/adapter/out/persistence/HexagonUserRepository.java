57dddae470cde55026be894f3ded8b333f8e8647
conagreen <icoqwee@gmail.com>
2021-11-25 22:19:10 +0900

    유저레포지토리 인터페이스 생성

---
+++ src/main/java/com/github/conagreen/hexagon/user/adapter/out/persistence/HexagonUserRepository.java
@@ -0,0 +1,14 @@
+package com.github.conagreen.hexagon.user.adapter.out.persistence;
+
+import com.github.conagreen.hexagon.user.domain.Email;
+import com.github.conagreen.hexagon.user.domain.HexagonUser;
+import com.github.conagreen.hexagon.user.domain.HexagonUserId;
+
+public interface HexagonUserRepository {
+
+    HexagonUser findByEmail(Email email);
+
+    HexagonUser findById(HexagonUserId hexagonUserId);
+
+    void save(HexagonUser hexagonUser);
+}
