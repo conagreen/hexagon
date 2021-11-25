00485dd4cac71308ed234a8de25b1190c333ca7c
conagreen <icoqwee@gmail.com>
2021-11-25 23:52:03 +0900

    어댑터 생성 - 두 개의 outbound port를 연결

---
+++ src/main/java/com/github/conagreen/hexagon/user/adapter/out/persistence/HexagonUserPersistenceAdapter.java
@@ -0,0 +1,31 @@
+package com.github.conagreen.hexagon.user.adapter.out.persistence;
+
+import com.github.conagreen.hexagon.user.application.port.out.LoadHexagonUserPort;
+import com.github.conagreen.hexagon.user.application.port.out.SaveHexagonUserPort;
+import com.github.conagreen.hexagon.user.domain.Email;
+import com.github.conagreen.hexagon.user.domain.HexagonUser;
+import com.github.conagreen.hexagon.user.domain.HexagonUserId;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+public class HexagonUserPersistenceAdapter implements SaveHexagonUserPort, LoadHexagonUserPort {
+
+    private final HexagonUserRepository hexagonUserRepository;
+
+    @Override
+    public HexagonUser loadByEmail(Email email) {
+        return hexagonUserRepository.findByEmail(email);
+    }
+
+    @Override
+    public HexagonUser loadById(HexagonUserId hexagonUserId) {
+        return hexagonUserRepository.findById(hexagonUserId);
+    }
+
+    @Override
+    public void save(HexagonUser hexagonUser) {
+        hexagonUserRepository.save(hexagonUser);
+    }
+}
