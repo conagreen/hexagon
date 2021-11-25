1882d1fdc95e083dfd39d8d7fd42c50f831db580
conagreen <icoqwee@gmail.com>
2021-11-25 23:38:07 +0900

    유저레포지토리 구현체로 인메모리 저장소 구현

--- src/main/java/com/github/conagreen/hexagon/user/adapter/out/persistence/InmemoryHexagonUserRepository.java
+++ src/main/java/com/github/conagreen/hexagon/user/adapter/out/persistence/InmemoryHexagonUserRepository.java
@@ -1,2 +1,35 @@
-package com.github.conagreen.hexagon.user.adapter.out.persistence;public class InmemoryHexagonUserRepository {
+package com.github.conagreen.hexagon.user.adapter.out.persistence;
+
+import com.github.conagreen.hexagon.user.domain.Email;
+import com.github.conagreen.hexagon.user.domain.HexagonUser;
+import com.github.conagreen.hexagon.user.domain.HexagonUserId;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class InmemoryHexagonUserRepository implements HexagonUserRepository {
+
+    private final Map<Email, HexagonUser> dataStore = new HashMap<>();
+
+    @Override
+    public HexagonUser findByEmail(Email email) {
+        return dataStore.get(email);
+    }
+
+    @Override
+    public HexagonUser findById(HexagonUserId hexagonUserId) {
+        for (HexagonUser hexagonUser : dataStore.values()) {
+            if (hexagonUser.getUserId().equals(hexagonUserId)) {
+                return hexagonUser;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public void save(HexagonUser hexagonUser) {
+        dataStore.put(hexagonUser.getUserProfile().getEmail(), hexagonUser);
+    }
 }
