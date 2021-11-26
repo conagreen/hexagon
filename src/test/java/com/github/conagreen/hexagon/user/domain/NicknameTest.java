12a515cce5cb31693c6b58f52b20f21d5e144f4e
conagreen <icoqwee@gmail.com>
2021-11-27 01:23:24 +0900

    사용자 닉네임 도메인 테스트 코드 작성

---
+++ src/test/java/com/github/conagreen/hexagon/user/domain/NicknameTest.java
@@ -0,0 +1,34 @@
+package com.github.conagreen.hexagon.user.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayName("사용자 닉네임 도메인 테스트")
+class NicknameTest {
+
+    @DisplayName("닉네임이 제약조건을 만족하는 경우")
+    @Test
+    void testNickname() {
+        assertDoesNotThrow(() -> new Nickname("닉네임"));
+    }
+
+    @DisplayName("닉네임이 null인 경우 IllegalArgumentException 발생")
+    @Test
+    void testNullNickname() {
+        assertThrows(IllegalArgumentException.class, () -> new Nickname(null));
+    }
+
+    @DisplayName("닉네임이 비어있는 경우 IllegalArgumentException 발생")
+    @Test
+    void testEmptyNickname() {
+        assertThrows(IllegalArgumentException.class, () -> new Nickname(""));
+    }
+
+    @DisplayName("닉네임이 공백으로만 되어 있는 경우 IllegalArgumentException 발생")
+    @Test
+    void testWhiteSpaceNickname() {
+        assertThrows(IllegalArgumentException.class, () -> new Nickname("     "));
+    }
+}
