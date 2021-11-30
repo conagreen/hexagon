9aaf292a1f0c8f1f4c19c29d5a9eb62be6505aee
conagreen <icoqwee@gmail.com>
2021-11-30 18:34:52 +0900

    Nickname, Email 도메인 테스트 코드 작성

---
+++ src/test/java/com/github/conagreen/hexagon/user/domain/EmailTest.java
@@ -0,0 +1,11 @@
+package com.github.conagreen.hexagon.user.domain;
+
+import org.junit.jupiter.api.DisplayName;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayName("사용자 이메일 도메인 테스트")
+class EmailTest {
+
+    @DisplayName("")
+}
--- src/test/java/com/github/conagreen/hexagon/user/domain/NicknameTest.java
+++ src/test/java/com/github/conagreen/hexagon/user/domain/NicknameTest.java
@@ -8,9 +8,9 @@ import static org.junit.jupiter.api.Assertions.*;
 @DisplayName("사용자 닉네임 도메인 테스트")
 class NicknameTest {
 
-    @DisplayName("닉네임이 제약조건을 만족하는 경우")
+    @DisplayName("닉네임이 제약조건을 만족하는 경우 어떠한 예외도 발생하지 않음")
     @Test
-    void testNickname() {
+    void testCorrectNickname() {
         assertDoesNotThrow(() -> new Nickname("닉네임"));
     }
 
