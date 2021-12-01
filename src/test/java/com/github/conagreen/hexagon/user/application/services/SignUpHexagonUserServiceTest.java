6963635b252d358f17a235590dedb30bd339bb8e
conagreen <icoqwee@gmail.com>
2021-12-01 23:46:46 +0900

    회원가입 서비스 테스트코드 추가 - 이미 존재하는 메일 주소일 경우

--- src/test/java/com/github/conagreen/hexagon/user/application/services/SignUpHexagonUserServiceTest.java
+++ src/test/java/com/github/conagreen/hexagon/user/application/services/SignUpHexagonUserServiceTest.java
@@ -3,6 +3,7 @@ package com.github.conagreen.hexagon.user.application.services;
 import com.github.conagreen.hexagon.user.adapter.out.persistence.HexagonUserPersistenceAdapter;
 import com.github.conagreen.hexagon.user.adapter.out.persistence.HexagonUserRepository;
 import com.github.conagreen.hexagon.user.adapter.out.persistence.InmemoryHexagonUserRepository;
+import com.github.conagreen.hexagon.user.application.port.in.EmailAlreadyTakenException;
 import com.github.conagreen.hexagon.user.application.port.in.SignUpHexagonUserCommand;
 import com.github.conagreen.hexagon.user.application.port.out.LoadHexagonUserPort;
 import com.github.conagreen.hexagon.user.application.port.out.SaveHexagonUserPort;
@@ -25,9 +26,8 @@ class SignUpHexagonUserServiceTest {
     @BeforeEach
     void beforeEach() {
         hexagonUserRepository = new InmemoryHexagonUserRepository();
-        LoadHexagonUserPort loadHexagonUserPort = new HexagonUserPersistenceAdapter(hexagonUserRepository);
-        SaveHexagonUserPort saveHexagonUserPort = new HexagonUserPersistenceAdapter(hexagonUserRepository);
-        signUpHexagonUserService = new SignUpHexagonUserService(loadHexagonUserPort, saveHexagonUserPort);
+        HexagonUserPersistenceAdapter persistenceAdapter = new HexagonUserPersistenceAdapter(hexagonUserRepository);
+        signUpHexagonUserService = new SignUpHexagonUserService(persistenceAdapter, persistenceAdapter);
     }
 
     @DisplayName("회원가입")
@@ -48,4 +48,30 @@ class SignUpHexagonUserServiceTest {
         HexagonUser result = hexagonUserRepository.findByEmail(new Email(command.getEmail()));
         assertEquals(result.getUserProfile().getEmail().getEmail(), command.getEmail());
     }
+
+    @DisplayName("이미 존재하는 메일 주소일 경우 EmailAlreadyTakenException 발생")
+    @Test
+    void checkDuplicateEmail() {
+        // given
+        SignUpHexagonUserCommand command1 = new SignUpHexagonUserCommand(
+                "test@hexgon.com",
+                "cona",
+                "https://avatars.githubusercontent.com/u/68418154?v=4",
+                "서버 개발자(가 되고싶어요)"
+        );
+
+        SignUpHexagonUserCommand command2 = new SignUpHexagonUserCommand(
+                "test@hexgon.com",
+                "미진",
+                "https://github.com/conagreen",
+                "서버 개발자(이고싶다)"
+        );
+
+        // when & then
+        signUpHexagonUserService.execute(command1);
+        assertThrows(EmailAlreadyTakenException.class,
+                () -> signUpHexagonUserService.execute(command2));
+
+    }
+
 }
